2d15436e3f449d2954d6ebeb2e3f16523879f20c
Christian Jaeger <chrjae@gmail.com>
2012-07-03 14:23:19 +0100

    add email to copyright notice

--- div.scm
+++ div.scm
@@ -1,8 +1,9 @@
-;; Copyright (c) 2012 by Christian Jaeger.  This is free software: you
-;; can redistribute it and/or modify it under the terms of the GNU
-;; Lesser General Public License as published by the Free Software
-;; Foundation, either version 2 of the License, or (at your option)
-;; any later version. See <http://www.gnu.org/licenses/>.
+;; Copyright (c) 2012 by Christian Jaeger <ch@christianjaeger.ch>.
+;; This is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU Lesser General Public License as
+;; published by the Free Software Foundation, either version 2 of the
+;; License, or (at your option) any later version. See
+;; <http://www.gnu.org/licenses/>.
 
 
 (define-macro (assert expr)
